26ee259c6a826b2e560a69b30feb406c05731ea0
IsaacAmp24 <ampudiaflores@gmailcom>
2024-05-21 01:54:02 -0500

    initial commit.

---
+++ android/app/src/main/kotlin/org/techteam/app/fastgym_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.techteam.app.fastgym_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/sceens/loginScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatelessWidget {
+  const loginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
