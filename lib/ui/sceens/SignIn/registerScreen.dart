5138d81c8124c8554786063c4b58785404729da9
IsaacAmp24 <ampudiaflores@gmailcom>
2024-06-11 13:32:27 -0500

    feat: added welcomeScreen.

--- lib/ui/sceens/SignIn/registerScreen.dart
+++ lib/ui/sceens/SignIn/registerScreen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
 import '../../widgets/loginWidget/customTextFormFlield.dart';
 
-
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({super.key});
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
@@ -16,165 +14,165 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-  TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
 
   bool _passwordVisible = false;
   bool _confirmPasswordVisible = false;
 
   void _register() {
-    // si el formulario es valido se guarda el estado del formulario
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print(
-          'Name: ${_nameController.text}, Email: ${_emailController.text}, Password: ${_passwordController.text}, Confirm Password: ${_confirmPasswordController.text}');
+      print('Name: ${_nameController.text}, Email: ${_emailController.text}, Password: ${_passwordController.text}, Confirm Password: ${_confirmPasswordController.text}');
+      Navigator.pushNamed(context, '/login');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Text(
-                      'Crea tu cuenta',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 0, 140, 255),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Crea tu cuenta',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 140, 255),
+                    ),
+                  ),
+                  const Text(
+                    '¡Bienvenido a Fast Gym!',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Color.fromARGB(255, 0, 140, 255),
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  CustomTextFormField(
+                    labelText: 'Nombre',
+                    icon: Icons.person,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa tu nombre';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _nameController.text = value!;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  CustomTextFormField(
+                    labelText: 'Email',
+                    icon: Icons.email,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa tu email';
+                      } else if (!value.contains('@')) {
+                        return 'Por favor ingresa un email válido';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _emailController.text = value!;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  CustomTextFormField(
+                    labelText: 'Contraseña',
+                    icon: Icons.lock,
+                    obscureText: !_passwordVisible,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible ? Icons.visibility : Icons.visibility_off,
                       ),
-                    ),
-                    const Text(
-                      '¡Bienvenido a Fast Gym!',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          color: Color.fromARGB(255, 0, 140, 255),
-                          fontWeight: FontWeight.w300),
-                    ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Nombre',
-                      icon: Icons.person,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingresa tu nombre';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _nameController.text = value!;
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
                       },
                     ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Email',
-                      icon: Icons.email,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingresa tu email';
-                        } else if (!value.contains('@')) {
-                          return 'Por favor ingresa un email válido';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _emailController.text = value!;
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa tu contraseña';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _passwordController.text = value!;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  CustomTextFormField(
+                    labelText: 'Confirmar contraseña',
+                    icon: Icons.lock,
+                    obscureText: !_confirmPasswordVisible,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _confirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _confirmPasswordVisible = !_confirmPasswordVisible;
+                        });
                       },
                     ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Contraseña',
-                      icon: Icons.lock,
-                      obscureText: !_passwordVisible,
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor confirma tu contraseña';
+                      } else if (value != _passwordController.text) {
+                        return 'Las contraseñas no coinciden';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _confirmPasswordController.text = value!;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _register,
+                      child: const Text('Registrarse'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 0, 140, 255),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _passwordVisible = !_passwordVisible;
-                          });
-                        },
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingresa tu contraseña';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _passwordController.text = value!;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Confirmar contraseña',
-                      icon: Icons.lock,
-                      obscureText: !_confirmPasswordVisible,
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _confirmPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _confirmPasswordVisible = !_confirmPasswordVisible;
-                          });
-                        },
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor confirma tu contraseña';
-                        } else if (value != _passwordController.text) {
-                          return 'Las contraseñas no coinciden';
-                        }
-                        return null;
-                      },
-
-                      onSaved: (value) {
-                        _confirmPasswordController.text = value!;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: _register,
-                        child: const Text('Registrarse'),
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 0, 140, 255),
-                          ),
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                            Colors.white,
-                          ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.white,
                         ),
                       ),
                     ),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/');
-                        },
-                        child: const Text('¿Ya tienes cuenta? Inicia sesión'),
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 0, 140, 255),
-                          ),
+                  ),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: const Text('¿Ya tienes cuenta? Inicia sesión'),
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 0, 140, 255),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            )));
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
