3d75ffe0093f0093067b2439d3d0ebaa7883d6e7
IsaacAmp24 <ampudiaflores@gmailcom>
2024-06-11 22:11:44 -0500

    feat(profile): added screen login validate and profile

--- lib/ui/sceens/SignIn/loginScreen.dart
+++ lib/ui/sceens/SignIn/loginScreen.dart
@@ -1,129 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../../widgets/loginWidget/customTextFormFlield.dart';
 
-
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // se define una clave global para el formulario
-  final _formKey = GlobalKey<FormState>();
-
-  // se definen los controladores para los campos de texto
-  // esto se hace para poder obtener el valor de los campos de texto
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
   bool _passwordVisible = false;
-  bool _confirmPasswordVisible = false;
 
-  void _login() {
+  void _login() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      // Proceso de inicio de sesión
-      print('Email: ${_emailController.text}, Password: ${_passwordController.text}');
 
-      // Navega al "hogar" de la aplicación
-      Navigator.pushReplacementNamed(context, '/home');
+      final email = _emailController.text;
+      final password = _passwordController.text;
+
+      final response = await http.get(Uri.parse('https://my-json-server.typicode.com/IsaacAmp24/users_fakeApi/users'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> users = json.decode(response.body);
+        final user = users.firstWhere(
+              (user) => user['email'] == email && user['password'] == password,
+          orElse: () => null,
+        );
+
+        if (user != null) {
+          Navigator.pushReplacementNamed(context, '/home');
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Credenciales inválidas'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error al iniciar sesión. Inténtalo de nuevo más tarde.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Text(
-                      'Inicia sesión',
-                      style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 0, 140, 255)),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Inicia sesión',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 140, 255),
+                  ),
+                ),
+                const Text(
+                  '¡Bienvenido a FastGym!',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color.fromARGB(255, 0, 140, 255),
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  labelText: 'Email',
+                  icon: Icons.email,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingresa tu email';
+                    } else if (!value.contains('@')) {
+                      return 'Por favor ingresa un email válido';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _emailController.text = value!;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  labelText: 'Contraseña',
+                  icon: Icons.lock,
+                  obscureText: !_passwordVisible,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _passwordVisible ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingresa tu contraseña';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _passwordController.text = value!;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _login,
+                    child: const Text('Iniciar sesión'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 0, 140, 255)),
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                     ),
-                    const Text(
-                      '¡Bienvenido a FastGym!',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          color: Color.fromARGB(255, 0, 140, 255),
-                          fontWeight: FontWeight.w300),
+                  ),
+                ),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: const Text('¿No tienes cuenta? Regístrate aquí'),
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 0, 140, 255)),
                     ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Email',
-                      icon: Icons.email,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingresa tu email';
-                        } else if (!value.contains('@')) {
-                          return 'Por favor ingresa un email válido';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _emailController.text = value!;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    CustomTextFormField(
-                      labelText: 'Contraseña',
-                      icon: Icons.lock,
-                      obscureText: !_passwordVisible,
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                            _passwordVisible ? Icons.visibility : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _passwordVisible = !_passwordVisible;
-                          });
-                        },
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingresa tu contraseña';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _passwordController.text = value!;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                          onPressed: _login,
-                          child: const Text('Iniciar sesión'),
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Color.fromARGB(255, 0, 140, 255)),
-                              foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white))),
-                    ),
-                    Center(
-                      child: TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/register');
-                          },
-                          child: const Text('¿No tienes cuenta? Regístrate aquí'),
-                          style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Color.fromARGB(255, 0, 140, 255)))),
-                    )
-                  ]),
+                  ),
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
